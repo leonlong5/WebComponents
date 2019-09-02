b2e3353c0b9828bf5a16abd5a4f04915894108f8
unknown <long.wang56@gmail.com>
2019-09-02 10:45:44 -0700

    debounce input widget

---
+++ problems/DebouncingThrottling.js
@@ -0,0 +1,5 @@
+// use cases
+// Wait until the user stops resizing the window
+// Don't fire an ajax event until the user stops typing
+// Measure the scroll position of the page and respond at most every 50ms
+// Ensure good performance as you drag elements around in an app
