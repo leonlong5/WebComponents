281cd1d70a6d89e610a4dc348c019a4f9e4c922d
Leon Wang <long.wang56@gmail.com>
2019-09-10 17:44:34 -0700

    complete basic functions

--- react/autocomplete/src/Autocomplete.js
+++ react/autocomplete/src/Autocomplete.js
@@ -13,18 +13,71 @@ class Autocomplete extends React.Component{
 
     }
 
+    fetchData () {
+        //clear the suggestions everytime user types
+        this.setState({
+            suggestions:[],
+            areSuggestionsVisiable: false
+        });
+
+        //no query
+        if (!this.state.query.length) return;
+
+        //debounce
+        clearTimeout(this.timeout);
+
+        //mimics a server call
+        this.timeout = setTimeout(()=>{
+            this.setState({
+                suggestions: [
+                    'Jack', 'Jason', "John", "Joy", "Jackson", "Josh"
+                ].filter(i => i.indexOf(this.state.query) !== -1), //use query to filter out
+                areSuggestionsVisiable: true,
+                selectedIndex: 0
+            })
+        }, 1000);
+    }
+
+    handleInputchange (e) {
+        const query = e.value;
+        this.setState({ query }, () => {
+            this.fetchData()
+        })
+    }
+
     render() {
         return (
             <div>
+                {this.state.selectedValue ? 
+                 <p>Slected Value: {this.state.selectedValue}</p> : null}
+
                 <input
                     type='text'
-                    onKeyUp={this.handleKeyPress.bind(this)} />
+                    onKeyUp={this.handleKeyPress.bind(this)} 
+                    onChange={this.handleInputchange.bind(this)}/>
 
-                <div calssName='suggestions'>
-                    <ul>
-                        
-                    </ul>
-                </div>
+
+                { (this.state.areSuggestionsVisiable && this.state.suggestions.length) ?
+                    
+                    <div calssName='suggestions'>
+                        <ul>
+                            {this.state.suggestions.map((item, i)=>{
+                                return <li
+                                        key={i}
+                                        className={i === this.state.selectedIndex ? 'selected' : ''}
+                                        onClick={()=>{ 
+                                            this.setState({
+                                                selectedValue: {item},
+                                                areSuggestionsVisiable:false
+                                            }) 
+                                        }}
+                                        >
+                                            {item}
+                                        </li>
+                            })}
+                        </ul>
+
+                </div> : null}
             </div>
         )
     }
